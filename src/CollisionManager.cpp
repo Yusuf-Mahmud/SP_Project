c865ce8a15b1ab9b29a75b412a8752aa558a92e6
AnasElwkel <159175102+AnsElwkel@users.noreply.github.com>
2024-04-02 06:42:35 +0200

    Update CollisionManager.cpp
    last version

--- src/CollisionManager.cpp
+++ src/CollisionManager.cpp
@@ -1,6 +1,6 @@
 #include "CollisionManager.h"
 
-void collisionManager1::setObjectBound(Sprite & a, Sprite & b){
+void collisionManager1::setObjectBound(Sprite& a, Sprite& b) {
 	ob1Bounds = a.getGlobalBounds(), ob2Bounds = b.getGlobalBounds();
 }
 //Testing
@@ -11,15 +11,23 @@ void collisionManager1::setObjectBound(RectangleShape& a, RectangleShape& b) {
 void collisionManager1::setObjectBound(RectangleShape& a, Sprite& b) {
 	ob1Bounds = a.getGlobalBounds(), ob2Bounds = b.getGlobalBounds();
 }
+void collisionManager1::setObjectBound(Sprite& a, RectangleShape& b) {
+	ob1Bounds = a.getGlobalBounds(), ob2Bounds = b.getGlobalBounds();
+}
 
 
 bool collisionManager1::isColliding(Sprite& a, Sprite& b)
-{ 
+{
 	setObjectBound(a, b);
 	return ob1Bounds.intersects(ob2Bounds);
 }
 bool collisionManager1::isColliding(RectangleShape& a, RectangleShape& b)
-{ 
+{
+	setObjectBound(a, b);
+	return ob1Bounds.intersects(ob2Bounds);
+}
+bool collisionManager1::isColliding(Sprite& a, RectangleShape& b)
+{
 	setObjectBound(a, b);
 	return ob1Bounds.intersects(ob2Bounds);
 }
@@ -34,10 +42,10 @@ float collisionManager1::calcOverlapY(FloatRect& a, FloatRect& b)
 	return min(a.top + a.height - b.top, b.top + b.height - a.top);
 }
 
-void collisionManager1::Movement(Sprite& a, Sprite& b){
-	
+void collisionManager1::Movement(Sprite& a, Sprite& b) {
+
 	Vector2f Dirction;
-	float xOverlap = calcOverlapX( ob1Bounds ,ob2Bounds );
+	float xOverlap = calcOverlapX(ob1Bounds, ob2Bounds);
 	float yOverlap = calcOverlapY(ob1Bounds, ob2Bounds);
 
 	if (xOverlap < yOverlap)
@@ -75,8 +83,8 @@ void collisionManager2::setNextBound(FloatRect& ob1Bounds, Vector2f& Velocity)
 	This Function is handling Collision by another approach (like as suraj tutorial approach)
 Parameters -> 1- walls (or anything that have colliding with object
 			  2- movementSpeed of object (To handle movement when colliding)
-			  3- objects to check that every object(like as cheep or enemy and so on...) 
-			     has been colliding with walls (or anything in map)
+			  3- objects to check that every object(like as cheep or enemy and so on...)
+				 has been colliding with walls (or anything in map)
 */
 void collisionManager2::applyCollision(vector<Sprite>& walls, Vector2f& Velocity, vector<Sprite>& objects)
 {
@@ -115,39 +123,6 @@ void collisionManager2::applyCollision(vector<Sprite>& walls, Vector2f& Velocity
 void collisionManager2::applyCollision(vector<RectangleShape>& walls, Vector2f& Velocity, RectangleShape& object)
 {
 	for (auto& wall : walls) {
-		setObjectBound(object,wall);
-		setNextBound(ob1Bounds, Velocity);
-		if (ob2Bounds.intersects(nextStep)) {
-			//Handle Left Collision
-			if (leftCollision()) {
-				Velocity.x = 0;
-				object.setPosition(ob2Bounds.left + ob2Bounds.width, object.getPosition().y);
-			}
-			//Handle Right Collision
-			else if (rightCollision()) {
-				Velocity.x = 0; 
-				object.setPosition(ob2Bounds.left - ob1Bounds.width, object.getPosition().y);
-			}
-
-			//Handle Top Collision
-			if (topCollision()) {
-				Velocity.y = 0;
-				object.setPosition(object.getPosition().x, ob2Bounds.top + ob2Bounds.height);
-			}
-			//Handle Bottom Collision
-			else if (bottomCollision()) {
-				Velocity.y = 0;
-				object.setPosition(object.getPosition().x, ob2Bounds.top - ob1Bounds.height);
-			}
-
-		}
-	}
-}
-
-//Testing 
-void collisionManager2::applyCollision(vector<Sprite>& walls, Vector2f& Velocity, RectangleShape& object)
-{
-	for (auto& wall : walls) {
 		setObjectBound(object, wall);
 		setNextBound(ob1Bounds, Velocity);
 		if (ob2Bounds.intersects(nextStep)) {
@@ -177,22 +152,55 @@ void collisionManager2::applyCollision(vector<Sprite>& walls, Vector2f& Velocity
 	}
 }
 
+//Testing 
+void collisionManager2::applyCollision(vector<Sprite>& walls, Vector2f& Velocity, RectangleShape& object)
+{
+	for (auto& wall : walls) {
+		setObjectBound(object, wall);
+		setNextBound(ob1Bounds, Velocity);
+		if (ob2Bounds.intersects(nextStep)) {
+			//Handle Left Collision
+			if (leftCollision()) {
+				Velocity.x = 0;
+				object.setPosition(ob2Bounds.left + ob2Bounds.width, ob1Bounds.top);
+			}
+			//Handle Right Collision
+			else if (rightCollision()) {
+				Velocity.x = 0;
+				object.setPosition(ob2Bounds.left - ob1Bounds.width, ob1Bounds.top);
+			}
+
+			//Handle Top Collision
+			if (topCollision()) {
+				Velocity.y = 0;
+				object.setPosition(ob1Bounds.left, ob2Bounds.top + ob2Bounds.height);
+			}
+			//Handle Bottom Collision
+			else if (bottomCollision()) {
+				Velocity.y = 0;
+				object.setPosition(ob1Bounds.left, ob2Bounds.top - ob1Bounds.height);
+			}
+
+		}
+	}
+}
+
 //Check the collision from Left side
 bool collisionManager2::leftCollision()
 {
 	return (ob1Bounds.left > ob2Bounds.left
-			&& ob1Bounds.left + ob1Bounds.width > ob2Bounds.left + ob2Bounds.width
-			&& ob1Bounds.top < ob2Bounds.top + ob2Bounds.height
-			&& ob2Bounds.top < ob1Bounds.top + ob2Bounds.height);
+		&& ob1Bounds.left + ob1Bounds.width > ob2Bounds.left + ob2Bounds.width
+		&& ob1Bounds.top < ob2Bounds.top + ob2Bounds.height
+		&& ob2Bounds.top < ob1Bounds.top + ob1Bounds.height);
 }
 
 //Check the collision from Right side
 bool collisionManager2::rightCollision()
 {
 	return (ob1Bounds.left < ob2Bounds.left
-			&& ob1Bounds.left + ob1Bounds.width < ob2Bounds.left + ob2Bounds.width
-			&& ob1Bounds.top < ob2Bounds.top + ob2Bounds.height
-			&& ob2Bounds.top < ob1Bounds.top + ob2Bounds.height);
+		&& ob1Bounds.left + ob1Bounds.width < ob2Bounds.left + ob2Bounds.width
+		&& ob1Bounds.top < ob2Bounds.top + ob2Bounds.height
+		&& ob2Bounds.top < ob1Bounds.top + ob1Bounds.height);
 }
 
 
@@ -200,18 +208,18 @@ bool collisionManager2::rightCollision()
 bool collisionManager2::topCollision()
 {
 	return (ob1Bounds.top > ob2Bounds.top
-			&& ob1Bounds.top + ob1Bounds.height > ob2Bounds.top + ob2Bounds.height
-			&& ob1Bounds.left < ob2Bounds.left + ob2Bounds.width
-			&& ob2Bounds.left < ob1Bounds.left + ob1Bounds.width);
+		&& ob1Bounds.top + ob1Bounds.height > ob2Bounds.top + ob2Bounds.height
+		&& ob1Bounds.left < ob2Bounds.left + ob2Bounds.width
+		&& ob2Bounds.left < ob1Bounds.left + ob1Bounds.width);
 }
 
 //Check the collision from Bottom 
 bool collisionManager2::bottomCollision()
 {
 	return (ob1Bounds.top < ob2Bounds.top
-			&& ob1Bounds.top + ob1Bounds.height < ob2Bounds.top + ob2Bounds.height
-			&& ob1Bounds.left < ob2Bounds.left + ob2Bounds.width
-			&& ob2Bounds.left < ob1Bounds.left + ob1Bounds.width);
+		&& ob1Bounds.top + ob1Bounds.height < ob2Bounds.top + ob2Bounds.height
+		&& ob1Bounds.left < ob2Bounds.left + ob2Bounds.width
+		&& ob2Bounds.left < ob1Bounds.left + ob1Bounds.width);
 }
 
 
@@ -237,3 +245,41 @@ bool collisionManager1::CollidingWithScreenBounderies(Window& window, Sprite& ob
 
 
 
+
+
+//Testing 
+void collisionManager2::applyCollisionWithAnimals(vector<Sprite>& walls, Vector2f& Velocity, RectangleShape& object)
+{
+	for (auto& wall : walls) {
+		setObjectBound(object, wall);
+		setNextBound(ob1Bounds, Velocity);
+		if (ob2Bounds.intersects(nextStep)) {
+			//Handle Left Collision
+			if (leftCollision()) {
+				//Velocity.x = 0;
+				object.setPosition(ob2Bounds.left + ob2Bounds.width, object.getPosition().y);
+				Velocity.x = -Velocity.x;
+			}
+			//Handle Right Collision
+			else if (rightCollision()) {
+				//Velocity.x = 0;
+				object.setPosition(ob2Bounds.left - ob1Bounds.width, object.getPosition().y);
+				Velocity.x = -Velocity.x;
+			}
+
+			//Handle Top Collision
+			if (topCollision()) {
+				//Velocity.y = 0;
+				object.setPosition(object.getPosition().x, ob2Bounds.top + ob2Bounds.height);
+				Velocity.y = -Velocity.y;
+			}
+			//Handle Bottom Collision
+			else if (bottomCollision()) {
+				//Velocity.y = 0;
+				object.setPosition(object.getPosition().x, ob2Bounds.top - ob1Bounds.height);
+				Velocity.y = -Velocity.y;
+			}
+
+		}
+	}
+}
